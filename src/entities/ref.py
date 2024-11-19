25b3bb437ef23464169686a88efae022e271ec8a
teemukoukkari <teemu.koukkari@gmail.com>
2024-11-19 09:52:10 +0200

    Update ref & ref_repository boilerplate

---
+++ src/entities/ref.py
@@ -0,0 +1,9 @@
+class Ref:
+    def __init__(self, id, type_, ref_name, author, title, year, publisher):
+        self.id = id
+        self.type = type_
+        self.ref_name = ref_name
+        self.author = author
+        self.title = title
+        self.year = year
+        self.publisher = publisher
---
+++ src/repositories/ref_repository.py
@@ -0,0 +1,27 @@
+from config import db
+from sqlalchemy import text
+
+from entities.Ref import Ref
+
+def get_refs():
+    result = db.session.execute(text("""
+        SELECT id, type, ref_name, author, title, year, publisher FROM refs
+    """))
+    refs = result.fetchall()
+    return [Ref(ref[0], ref[1], ref[2], ref[3], ref[4], ref[5], ref[6]) for ref in refs] 
+
+def create_ref(ref: Ref):
+    sql = text("""
+        INSERT INTO todos (id, type, ref_name, author, title, year, publisher)
+        VALUES (:id, :type, :ref_name, :author, :title, :year, :publisher)
+    """)
+    db.session.execute(sql, { 
+        "id": ref.id,
+        "type": ref.type,
+        "ref_name": ref.ref_name,
+        "author": ref.author,
+        "title": ref.title,
+        "year": ref.year,
+        "publisher": ref.publisher
+    })
+    db.session.commit()
