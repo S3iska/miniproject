32468656be2ef762eef95b02fc3f332f8ff06066
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-11-19 09:55:50 +0200

    whitespaces

--- src/repositories/ref_repository.py
+++ src/repositories/ref_repository.py
@@ -3,19 +3,22 @@ from sqlalchemy import text
 
 from entities.Ref import Ref
 
+
 def get_refs():
     result = db.session.execute(text("""
-        SELECT id, type, ref_name, author, title, year, publisher FROM refs
+        SELECT id, type, ref_name, author, title, year, publisher
+        FROM refs
     """))
     refs = result.fetchall()
     return [Ref(ref[0], ref[1], ref[2], ref[3], ref[4], ref[5], ref[6]) for ref in refs] 
+
 
 def create_ref(ref: Ref):
     sql = text("""
         INSERT INTO todos (id, type, ref_name, author, title, year, publisher)
         VALUES (:id, :type, :ref_name, :author, :title, :year, :publisher)
     """)
-    db.session.execute(sql, { 
+    db.session.execute(sql, {
         "id": ref.id,
         "type": ref.type,
         "ref_name": ref.ref_name,
