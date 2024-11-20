13819670044617468e774b4007afc6f3806563a5
teemukoukkari <teemu.koukkari@gmail.com>
2024-11-20 10:51:36 +0200

    Fix some style

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,34 +1,34 @@
-from config import db, app
 from sqlalchemy import text
+from config import db, app
 
 def table_exists(name):
-  sql_table_existence = text(
-    "SELECT EXISTS ("
-    "  SELECT 1"
-    "  FROM information_schema.tables"
-    f" WHERE table_name = '{name}'"
-    ")"
-  )
+    sql_table_existence = text(
+        "SELECT EXISTS ("
+        "  SELECT 1"
+        "  FROM information_schema.tables"
+        f" WHERE table_name = '{name}'"
+        ")"
+    )
 
-  result = db.session.execute(sql_table_existence)
-  return result.fetchall()[0][0]
+    result = db.session.execute(sql_table_existence)
+    return result.fetchall()[0][0]
 
 def setup_db():
-  if table_exists("refs"):
-    sql = text(f"DROP TABLE refs;")
+    if table_exists("refs"):
+        sql = text("DROP TABLE refs;")
+        db.session.execute(sql)
+        db.session.commit()
+
+    sql = text("SELECT 1;")
+    with open('schema.sql', 'r', encoding="utf-8") as file:
+        sql = text(file.read())
+
     db.session.execute(sql)
     db.session.commit()
 
-  sql = text("SELECT 1;")
-  with open('schema.sql', 'r') as file:
-    sql = text(file.read())
-
-  db.session.execute(sql)
-  db.session.commit()
-
 def reset_db():
-  setup_db()
+    setup_db()
 
 if __name__ == "__main__":
     with app.app_context():
-      setup_db()
+        setup_db()
