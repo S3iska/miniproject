f5addd20dd92a96dc030c26f3430dba826899a9f
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-11-20 10:57:41 +0200

    route logic back to app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import redirect, render_template, request, jsonify, flash
 from db_helper import reset_db
 from config import app, test_env
-from add import add_ref
+from util import validate_ref
+from repositories.ref_repository import create_ref
 
 
 @app.route("/")
@@ -21,4 +22,19 @@ if test_env:
 def route_add():
     if request.method == "GET":
         return render_template("add.html")
-    return add_ref(request)
+    ref_type = request.form.get("type")
+    ref_author = request.form.get("author")
+    ref_title = request.form.get("title")
+    ref_year = int(request.form.get("year"))
+    ref_publisher = request.form.get("publisher")
+    ref_name = request.form.get("refname")
+
+    new_ref = Ref(None, ref_type, ref_name, ref_author, ref_title, ref_year, ref_publisher)
+
+    try:
+        validate_ref(new_ref)
+        create_ref(new_ref)
+    except Exception:
+        return "Could not add reference"
+
+    return redirect("/")
