4562504a81ce8285ecbd6e15cc776df0975f0003
teemukoukkari <teemu.koukkari@gmail.com>
2024-11-20 10:40:18 +0200

    Update db_helper.py

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,8 +1,6 @@
 from config import db, app
 from sqlalchemy import text
 
-table_name = "refs"
-
 def table_exists(name):
   sql_table_existence = text(
     "SELECT EXISTS ("
@@ -12,40 +10,24 @@ def table_exists(name):
     ")"
   )
 
-  print(f"Checking if table {name} exists")
-  print(sql_table_existence)
-
   result = db.session.execute(sql_table_existence)
   return result.fetchall()[0][0]
 
-def reset_db():
-  print(f"Clearing contents from table {table_name}")
-  sql = text(f"DELETE FROM {table_name}")
-  db.session.execute(sql)
-  db.session.commit()
-
 def setup_db():
-  if table_exists(table_name):
-    print(f"Table {table_name} exists, dropping")
-    sql = text(f"DROP TABLE {table_name}")
+  if table_exists("refs"):
+    sql = text(f"DROP TABLE refs;")
     db.session.execute(sql)
     db.session.commit()
 
-  print(f"Creating table {table_name}")
-  sql = text(
-    """CREATE TABLE refs (
-        id SERIAL PRIMARY KEY,
-        type TEXT NOT NULL,
-        refname TEXT NOT NULL,
-        author TEXT NOT NULL,
-        title TEXT NOT NULL,
-        year INTEGER NOT NULL,
-        publisher TEXT
-    );"""
-  )
+  sql = text("SELECT 1;")
+  with open('schema.sql', 'r') as file:
+    sql = text(file.read())
 
   db.session.execute(sql)
   db.session.commit()
+
+def reset_db():
+  setup_db()
 
 if __name__ == "__main__":
     with app.app_context():
