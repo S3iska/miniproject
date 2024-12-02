b2b829ea1b42fe4f7035198a96ab771a58b5a9a1
Lauri Kaijansinkko <lauri.kaijansinkko@helsinki.fi>
2024-12-02 17:50:50 +0200

    validate tag and testing

---
+++ src/tests/tag_test.py
@@ -0,0 +1,40 @@
+import unittest
+from entities.tag import Tag, validate_tag, UserInputError
+
+
+class TestTag(unittest.TestCase):
+    def test_validate_accepts_good_tag(self):
+        tag = Tag(1, "abc")
+        validate_tag(tag)
+
+    def test_validate_fails_tag_without_id(self):
+        tag = Tag(None, "abc")
+        try:
+            validate_tag(tag)
+            self.fail()
+        except UserInputError:
+            self.assertTrue(True)
+
+    def test_validate_fails_tag_with_too_short_name(self):
+        tag = Tag(1, "ab")
+        try:
+            validate_tag(tag)
+            self.fail()
+        except UserInputError:
+            self.assertTrue(True)
+
+    def test_validate_fails_tag_without_name(self):
+        tag = Tag(1, None)
+        try:
+            validate_tag(tag)
+            self.fail()
+        except UserInputError:
+            self.assertTrue(True)
+
+    def test_validate_fails_tag_with_invalid_name(self):
+        tag = Tag(1, "@bc")
+        try:
+            validate_tag(tag)
+            self.fail()
+        except UserInputError:
+            self.assertTrue(True)
