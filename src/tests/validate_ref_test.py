81ec13d5247b792eceff26cea3dfaff6081d1144
teemukoukkari <teemu.koukkari@gmail.com>
2024-12-04 13:54:33 +0200

    Update unit tests for ref validation a bit

--- src/tests/validate_ref_test.py
+++ src/tests/validate_ref_test.py
@@ -5,21 +5,34 @@ from util import UserInputError
 
 class TestRefValidation(unittest.TestCase):
     def setUp(self):
-        self.valid_ref = Ref(
+        self.valid_article = Ref(
             id = None,
             ref_type = "article",
             ref_name = "REF65",
-            author = "test author",
-            title = "test title",
+            author = "Test author",
+            title = "Test title",
             year = 1997,
-            journal = "test publisher"
+            journal = "Test journal"
         )
 
-    def test_valid_ref_does_not_raise_error(self):
-        self.valid_ref.validate()
+        self.valid_inproceedings = Ref(
+            id = None,
+            ref_type = "inproceedings",
+            ref_name = "REF65",
+            author = "Test author",
+            booktitle = "Test booktitle",
+            year = 1997,
+            publisher = "Test publisher"
+        )
+
+    def test_valid_article_ref_does_not_raise_error(self):
+        self.valid_article.validate()
+    
+    def test_valid_inproceedings_ref_does_not_raise_error(self):
+        self.valid_inproceedings.validate()
     
     def test_missing_or_invalid_ref_type_raises_error(self):
-        ref = copy(self.valid_ref)
+        ref = copy(self.valid_article)
 
         ref.ref_type = None
         with self.assertRaises(UserInputError):
@@ -30,7 +43,7 @@ class TestRefValidation(unittest.TestCase):
             ref.validate()
     
     def test_missing_or_too_long_ref_name_raises_error(self):
-        ref = copy(self.valid_ref)
+        ref = copy(self.valid_article)
 
         ref.ref_name = None
         with self.assertRaises(UserInputError):
@@ -41,7 +54,7 @@ class TestRefValidation(unittest.TestCase):
             ref.validate()
 
     def test_missing_or_too_short_or_long_author_raises_error(self):
-        ref = copy(self.valid_ref)
+        ref = copy(self.valid_article)
 
         ref.author = None
         with self.assertRaises(UserInputError):
@@ -56,22 +69,67 @@ class TestRefValidation(unittest.TestCase):
             ref.validate()
     
     def test_missing_or_too_short_or_long_title_raises_error(self):
-        ref = copy(self.valid_ref)
+        ref = copy(self.valid_article)
 
-        ref.author = None
+        ref.title = None
         with self.assertRaises(UserInputError):
             ref.validate()
         
-        ref.author = "a"*2
+        ref.title = "a"*2
         with self.assertRaises(UserInputError):
             ref.validate()
 
-        ref.author = "a"*251
+        ref.title = "a"*251
         with self.assertRaises(UserInputError):
             ref.validate()
     
+    def test_missing_or_too_short_or_long_booktitle_raises_error(self):
+        ref = copy(self.valid_inproceedings)
+
+        ref.booktitle = None
+        with self.assertRaises(UserInputError):
+            ref.validate()
+        
+        ref.booktitle = "a"*2
+        with self.assertRaises(UserInputError):
+            ref.validate()
+
+        ref.booktitle = "a"*251
+        with self.assertRaises(UserInputError):
+            ref.validate()
+    
+    def test_missing_or_too_short_or_long_journal_raises_error(self):
+        ref = copy(self.valid_article)
+
+        #ref.journal = None
+        #with self.assertRaises(UserInputError):
+        #    ref.validate()
+        
+        ref.journal = "a"*2
+        with self.assertRaises(UserInputError):
+            ref.validate()
+
+        ref.journal = "a"*251
+        with self.assertRaises(UserInputError):
+            ref.validate()
+    
+    def test_missing_or_too_short_or_long_publisher_raises_error(self):
+        ref = copy(self.valid_inproceedings)
+
+        #ref.publisher = None
+        #with self.assertRaises(UserInputError):
+        #    ref.validate()
+        
+        ref.publisher = "a"*2
+        with self.assertRaises(UserInputError):
+            ref.validate()
+
+        ref.publisher = "a"*251
+        with self.assertRaises(UserInputError):
+            ref.validate()
+
     def test_missing_or_invalid_year_raises_error(self):
-        ref = copy(self.valid_ref)
+        ref = copy(self.valid_article)
 
         ref.year = None
         with self.assertRaises(UserInputError):
